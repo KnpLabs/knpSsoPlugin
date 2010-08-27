d5c078f2f432f7954e3a99b649af6ebb422d28fd
Matthieu Bontemps <matthieu@knplabs.com>
2010-08-27 18:51:33 +0200

    Do not redirect after successful login

--- modules/knpSsoAuth/lib/BaseknpSsoAuthActions.class.php
+++ modules/knpSsoAuth/lib/BaseknpSsoAuthActions.class.php
@@ -32,16 +32,9 @@ class BaseknpSsoAuthActions extends sfActions
         $values = $this->form->getValues();
         $this->getUser()->signin($values['user'], array_key_exists('remember', $values) ? $values['remember'] : false);
 
-        // always redirect to a URL set in app.yml
-        // or to the referer
-        // or to the homepage
-        $signinUrl = sfConfig::get('app_knp_sso_plugin_success_signin_url', $user->getReferer($request->getReferer()));
-
-        return $this->redirect('' != $signinUrl ? $signinUrl : '@homepage');
       }
       else
       {
-        echo $this->form;
         return sfView::ERROR;
       }
     }
